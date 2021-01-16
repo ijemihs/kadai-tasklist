ffb20b0facb8a9b6c7083f3b2847436dfd60284e
shirokuma <my_determination_210292@yahoo.co.jp>
2021-01-16 12:40:38 +0000

    user registration

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -28,5 +28,13 @@
     
     {{-- タスク作成ページへのリンク --}}
     {!! link_to_route('tasks.create', '新規タスクの登録', [], ['class' => 'btn btn-primary']) !!}
+    
+    <div class="jumbotron">
+        <div class="text-center">
+            <h1>Tasklistへようこそ！</h1>
+            {{-- ユーザ登録ページへのリンク --}}
+            {!! link_to_route('signup.get', 'さあユーザ登録しましょう！', [], ['class' => 'btn btn-lg btn-primary']) !!}
+        </div>
+    </div>
 
 @endsection
